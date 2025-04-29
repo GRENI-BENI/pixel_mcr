52f55409b313794ae2cef85114621e68faa9291a
v.miloserdov_dragon- <miloserdov.vadik@gmail.com>
2025-04-29 18:30:24 +0300

    Final changes before deploy

--- gatewayserver/src/main/java/com/vady/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/vady/gatewayserver/config/SecurityConfig.java
@@ -18,19 +18,51 @@ import java.util.Arrays;
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) {
-        serverHttpSecurity.authorizeExchange(exchanges -> exchanges
-                                .pathMatchers("/**").permitAll()
-//                     .pathMatchers("/api/comments/**").authenticated()
-//                     .pathMatchers("/api/iam/**").permitAll()
-                )
-                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
-        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
-        serverHttpSecurity.cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource()));
+//    @Bean
+//    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+//        serverHttpSecurity.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated()
+//                     .pathMatchers(HttpMethod.GET,"/api/comments/**").permitAll()
+//                     .pathMatchers("/api/iam/auth/**").permitAll()
+//
+//                )
+//                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+//        serverHttpSecurity.csrf(ServerHttpSecurity.CsrfSpec::disable);
+//        serverHttpSecurity.cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource()));
+//
+//        return serverHttpSecurity.build();
+//    }
+@Bean
+public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+    serverHttpSecurity.authorizeExchange(exchanges -> exchanges
+                    // Auth endpoints - public access
+                    .pathMatchers("/api/iam/auth/**").permitAll()
 
-        return serverHttpSecurity.build();
-    }
+                    // Comment endpoints - GET is public, POST requires authentication
+                    .pathMatchers(HttpMethod.GET, "/api/comments/**").permitAll()
+
+                    // User endpoints - some public, some protected
+                    .pathMatchers(HttpMethod.GET, "/api/iam/user/nickname/**").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/iam/user/keycloak/**").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/iam/user/*/followers", "/api/iam/user/*/following").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/iam/user/batch-get").permitAll()
+
+                    // Photo endpoints - GET is mostly public
+                    .pathMatchers(HttpMethod.GET, "/api/photos/**").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/photos/trending").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/photos/tags").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/api/photos/user/**").permitAll()
+
+                    // All other requests require authentication
+                    .anyExchange().authenticated()
+            )
+            .oauth2ResourceServer(oAuth2ResourceServerSpec ->
+                    oAuth2ResourceServerSpec.jwt(Customizer.withDefaults())
+            )
+            .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .cors(corsSpec -> corsSpec.configurationSource(corsConfigurationSource()));
+
+    return serverHttpSecurity.build();
+}
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
